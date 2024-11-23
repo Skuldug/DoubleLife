04a66796e44507b00577eab6da2bbe3c2ffc2641
Cashisking247 <weeveltroaty@gmail.com>
2024-11-23 01:46:53 +0100

    Update basic_monster.gd

--- basic_monster.gd
+++ basic_monster.gd
@@ -1,1 +1,28 @@
 extends CharacterBody2D
+
+var run_speed = 25
+var chase = false
+var health = 10
+var target = null
+var damage = 20
+
+func _physics_process(delta) -> void:
+	velocity = Vector2.ZERO
+	if target:
+		velocity = position.direction_to(target.position) * run_speed
+	move_and_slide()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	target = body
+	if target.is_in_group("Player"):
+		body = target
+
+
+func _on_swing_body_entered(body: Node2D) -> void:
+	target = body
+	target.health -= 10
+	func _on_timer_timeout() -> void:
+		target.health -= 10
+		print (target.health)
+
+func _on_swing_body_exited(body: Node2D) -> void:
