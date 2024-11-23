00ff831894f57ca84cef13a151a2e6b5bb439403
Cashisking247 <weeveltroaty@gmail.com>
2024-11-23 02:37:42 +0100

    Latest epic upadte

--- basic_monster.gd
+++ basic_monster.gd
@@ -5,12 +5,13 @@ var chase = false
 var health = 10
 var target = null
 var damage = 20
+@onready var timer = $Timer
 
 func _physics_process(delta) -> void:
 	velocity = Vector2.ZERO
 	if target:
 		velocity = position.direction_to(target.position) * run_speed
-	move_and_slide()
+	move_and_collide(velocity)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	target = body
@@ -21,8 +22,9 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 func _on_swing_body_entered(body: Node2D) -> void:
 	target = body
 	target.health -= 10
-	func _on_timer_timeout():
-		target.health -= 10
-		print (target.health)
+func _on_timer_timeout() -> void:
+	target.health -= 10
+	print (target.health)
 
 func _on_swing_body_exited(body: Node2D) -> void:
+	pass
